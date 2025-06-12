d122001664e003d21553e4c7a4428944ff0a3533
Sham Saravaiahgari <sham.saravai@gmail.com>
2025-06-12 15:17:58 +0200

    updated the model

--- models/dish_model.js
+++ models/dish_model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const dishSchema = new mongoose.Schema({
-  name: { type: String, unique: true, required: true },
+  name: String,
   ingredients: [String],
   preparationSteps: [String],
   cookingTime: String,
   origin: String,
-  spiceLevel: String, // custom field
+  spiceLevel: String
 });
 
 module.exports = mongoose.model('Dish', dishSchema);
