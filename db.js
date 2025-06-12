5e32633e9fa1a4ce365408b4cf889e92053ef589
Sham Saravaiahgari <sham.saravai@gmail.com>
2025-06-12 15:15:32 +0200

    Update db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config(); // Load .env
-
-const uri = process.env.MONGO_URI;
-
-mongoose.connect(uri)
-  .then(() => console.log('✅ Connected to MongoDB'))
-  .catch(err => console.error('❌ MongoDB connection error:', err));
